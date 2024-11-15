bf3f1d6a39277dff14eb241c9274845483372cf2
Jen <145509868+Jendunsmore@users.noreply.github.com>
2024-11-15 14:28:40 -0500

    Built component: Quiz.jsx

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,16 @@
+// src/ components/ Quiz.jsx
+// this handles the "quiz" logic which includes fetching questions
+// managing the current question index
+
+import React from 'react';
+
+const Quiz = () => {
+    return (
+        <div>
+            <h2>Spooky Quiz</h2>
+            <p>Get ready to test your Halloween knowledge!</p>
+        </div>
+    );
+};
+
+export default Quiz;
