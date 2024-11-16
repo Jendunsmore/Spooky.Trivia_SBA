9b8ea8ae7fc083fa102d4e4fd835949530589389
Jen <145509868+Jendunsmore@users.noreply.github.com>
2024-11-16 00:08:30 -0500

    updated App.jsx, added Quiz, Question, Score, and FunFact imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 // Main component!!
 
-import React from 'react';
+import React, { useState } from 'react';
+import Quiz from './components/Quiz';
+import Question from './components/Question';
+import Score from './components/Score';
 
 const App = () => {
+  const [showQuiz, setShowQuiz] = useState(true);
+  const [score, setScore] = useState(0);
+
   return (
     <div>
       <h1>Welcome to the Spooky Trivia App</h1>
+      {showQuiz ? (
+        <Quiz />
+      ) : (
+        <Score score={score} />
+      )}
     </div>
   );
 };
